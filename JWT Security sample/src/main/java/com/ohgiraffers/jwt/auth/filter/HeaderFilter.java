3d15f3668191f8e982c060c234fe958626423686
yanghyeyeon <yhy4058@gmail.com>
2024-09-06 14:33:07 +0900

    JWT Security

--- JWT Security sample/src/main/java/com/ohgiraffers/jwt/auth/filter/HeaderFilter.java
+++ JWT Security sample/src/main/java/com/ohgiraffers/jwt/auth/filter/HeaderFilter.java
@@ -7,12 +7,27 @@ import java.io.IOException;
 import java.util.logging.LogRecord;
 
 
+/*
+* CORS 설정을 위한 Filter
+*
+* CORS (Cross-Origin_Resources-Sharing)
+* - 웹 브라우저에서 서로 다른 도메인 간의 자원 공유를 허용하거나 제한하기 위한 보안 메커니즘
+*
+* 동일출처정책 : 기본적으로 브라우저는 다른 도메인간의 리소스 요청을 허용하지 않는다.
+* CORS의 역할 : 서버가 허용한 특정 도메인에서만 자원을 요청할 수 있도록 설정
+*
+* CORS 해결 방법
+* 1. 서버에서 CORS 설정
+* - 서버가 요청을 허용할 도메인과 메서드, 헤더등을 명시적으로 작성
+* 2. 프록시 서버 사용
+* - 클라이언트와 서버 사이에 프록시를 사용해 cors 정책을 우회
+* */
 public class HeaderFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");  //다른 외부 요청의 응답을 허용할 것 인가?
+        res.setHeader("Access-Control-Allow-Origin", "*");  // 모든 출처에서의 요청을 허용
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");  //외부 요청에 허용할 메서드
         res.setHeader("Access-Control-Max-Age", "3600"); // 캐싱을 허용할 시간
         res.setHeader("Access-Control-Allow-Headers",
